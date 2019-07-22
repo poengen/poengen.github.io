ea410068fc9b6d6ea28ef41070e1f8db142ab83e
poengen <pal.oskar.engen@gmail.com>
2019-07-22 16:57:00 +0200

    new frontpage, html and css structure, and some javascript

---
+++ myScript.js
@@ -0,0 +1,5 @@
+function myFunction() {
+	document.getElementById('overskrift').textContent = 'Panda!';
+	document.getElementById('overskrift').style.color = 'Green';
+	document.getElementById('panda-card').style.display = 'block';
+}
