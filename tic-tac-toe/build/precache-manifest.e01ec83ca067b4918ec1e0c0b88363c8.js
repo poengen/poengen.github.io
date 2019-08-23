ca11680a7b94e6fd3baf45ea93ace81f35dca79c
poengen <pal.oskar.engen@gmail.com>
2019-08-23 15:17:34 +0200

    remove build for gitignore

---
+++ tic-tac-toe/build/precache-manifest.e01ec83ca067b4918ec1e0c0b88363c8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "93fa229f01eff72b4c152ba3c285319c",
+    "url": "/tic-tac-toe/index.html"
+  },
+  {
+    "revision": "bc59f95c8739c6007e88",
+    "url": "/tic-tac-toe/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "30f2cee13d68b3ef05c7",
+    "url": "/tic-tac-toe/static/js/2.88747c88.chunk.js"
+  },
+  {
+    "revision": "bc59f95c8739c6007e88",
+    "url": "/tic-tac-toe/static/js/main.8c6d3d9c.chunk.js"
+  },
+  {
+    "revision": "2447e331fa6994ee754e",
+    "url": "/tic-tac-toe/static/js/runtime~main.96c826cc.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/tic-tac-toe/static/media/logo.5d5d9eef.svg"
+  }
+]);
