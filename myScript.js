fe210947a77a92edeac0c6c29e8c3802d2478c6f
poengen <pal.oskar.engen@gmail.com>
2020-01-12 16:08:58 +0100

    new website

--- myScript.js
+++
@@ -1,5 +0,0 @@
-function myFunction() {
-  document.getElementById('overskrift').textContent = 'Panda!';
-  document.getElementById('overskrift').style.color = 'Green';
-  document.getElementById('panda-card').style.display = 'block';
-}
---
+++ src/components/introduction.jsx
@@ -0,0 +1,114 @@
+import React, { Component } from 'react';
+
+export default class Introduction extends Component {
+  render() {
+    return (
+      <div>
+        <section
+          id="colorlib-hero"
+          className="js-fullheight"
+          data-section="home"
+        >
+          <div className="flexslider js-fullheight">
+            <ul className="slides">
+              <li
+                style={{
+                  backgroundImage: 'url(images/img_bg.jpg)',
+                }}
+              >
+                <div className="overlay" />
+                <div className="container-fluid">
+                  <div className="row">
+                    <div className="col-md-6 col-md-offset-3 col-md-pull-3 col-sm-12 col-xs-12 js-fullheight slider-text">
+                      <div className="slider-text-inner js-fullheight">
+                        <div className="desc">
+                          <h1>
+                            Hi! <br />I'm Pål Oskar
+                          </h1>
+                          <p>
+                            <a
+                              className="btn btn-primary btn-learn"
+                              href="https://www.dropbox.com/s/9094gbfu1x6bdw6/CV_shared.pdf?dl=0"
+                              target="_blank"
+                              rel="noopener noreferrer"
+                            >
+                              View my consultant CV{' '}
+                              <i className="icon-download4" />
+                            </a>
+                          </p>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </li>
+              <li
+                style={{
+                  backgroundImage: 'url(images/img_bg.jpg)',
+                }}
+              >
+                <div className="overlay" />
+                <div className="container-fluid">
+                  <div className="row">
+                    <div className="col-md-6 col-md-offset-3 col-md-pull-3 col-sm-12 col-xs-12 js-fullheight slider-text">
+                      <div className="slider-text-inner">
+                        <div className="desc">
+                          <h1>
+                            I love building<br />
+                            software!
+                          </h1>
+                          <p>
+                            <a
+                              className="btn btn-primary btn-learn"
+                              href="https://github.com/poengen"
+                              target="_blank"
+                              rel="noopener noreferrer"
+                            >
+                              View Projects{' '}
+                              <i className="icon-briefcase3" />
+                            </a>
+                          </p>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </li>
+              <li
+                style={{
+                  backgroundImage: 'url(images/img_bg.jpg)',
+                }}
+              >
+                <div className="overlay" />
+                <div className="container-fluid">
+                  <div className="row">
+                    <div className="col-md-6 col-md-offset-3 col-md-pull-3 col-sm-12 col-xs-12 js-fullheight slider-text">
+                      <div className="slider-text-inner">
+                        <div className="desc">
+                          <h1>
+                            I sometimes <br />write...{' '}
+                          </h1>
+                          <p>
+                            <a
+                              className="btn btn-primary btn-learn"
+                              href="https://medium.com/@ploskarengen"
+                              target="_blank"
+                              rel="noopener noreferrer"
+                            >
+                              View Blog{' '}
+                              <i className="icon-book" />
+                            </a>
+                          </p>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </li>
+            </ul>
+          </div>
+        </section>
+      </div>
+    );
+  }
+}
